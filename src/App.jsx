3bb5033430118a399f9af68c77d50ea30ec96a31
Hon chow <hello@honchow.com>
2025-11-23 01:40:23 +0000

    implement dock

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Navbar, Welcome } from "#components";
+import { Navbar, Welcome, Dock } from "#components";
 
 export const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,17 @@ export default defineConfig({
     alias: {
       "#components": resolve(
         dirname(fileURLToPath(import.meta.url)),
-        "src/components"
+        "src/components",
       ),
       "#constants": resolve(
         dirname(fileURLToPath(import.meta.url)),
-        "src/constants"
+        "src/constants",
       ),
       "#store": resolve(dirname(fileURLToPath(import.meta.url)), "src/store"),
       "#hoc": resolve(dirname(fileURLToPath(import.meta.url)), "src/hoc"),
       "#windows": resolve(
         dirname(fileURLToPath(import.meta.url)),
-        "src/windows"
+        "src/windows",
       ),
     },
   },
